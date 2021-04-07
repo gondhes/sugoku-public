35cc1b347e4bdaedcd1aa73e69e773495aa97717
gondhes <fajarwirazdi@gmail.com>
2021-04-07 11:56:48 +0700

    add solve button

--- sugoku/screens/Game.js
+++ sugoku/screens/Game.js
@@ -46,6 +46,26 @@ export default function Game() {
       .catch(err => console.log(err))
   }
 
+  const solve = () => {
+    const answer = { board: board }
+    const encodeBoard = (board) => board.reduce((result, row, i) => result + `%5B${encodeURIComponent(row)}%5D${i === board.length - 1 ? '' : '%2C'}`, '')
+    const encodeParams = (params) =>
+      Object.keys(params)
+          .map(key => key + '=' + `%5B${encodeBoard(params[key])}%5D`)
+          .join('&')
+
+    axios({
+      url: 'https://sugoku.herokuapp.com/solve',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encodeParams(answer)
+    })
+      .then(({ data }) => {
+        setBoard(data.solution)
+      })
+      .catch(err => console.log(err))
+  }
+
   return (
       <View style={ styles.container }>
         <Text style={ styles.header }> SUGOKU </Text>
@@ -72,6 +92,7 @@ export default function Game() {
           }) 
         }
         <Button title='VALIDATE' onPress={validate}/>
+        <Button title='SOLVE' onPress={solve}/>
       </View>
   );
 }
